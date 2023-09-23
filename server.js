2ea6970eadbf8100e192ff297d5d92108de0549d
Lia Kim <j.liakim23@gmail.com>
2023-09-23 11:21:53 -0400

    changed port to 3000

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/about', (req,res) => {
     res.json(about);
 });
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
